3b394cdc4e82c6ae96be1d38c0e9b15327b51b30
Tarik Vu <Tarikvu2@yahoo.com>
2024-06-12 13:38:43 -0600

    Making a basic component in redux

--- redux-introduction/src/app/store.js
+++ redux-introduction/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore(
-    {
+    {  
         reducer: {
-
+            // Mount our reducers to the store
+            counter: counterReducer
         }
     }
 )
