4d785718a5b57ccf5623e2630a8726737148d133
Tarik Vu <Tarikvu2@yahoo.com>
2024-06-14 10:31:59 -0600

    Finished counter moving to a sub dir

--- redux-introduction/src/features/counter/counterSlice.js
+++ redux-introduction/src/features/counter/counterSlice.js
@@ -11,10 +11,16 @@ export const counterSlice = createSlice({
     // Reducer for state changes:
     reducers: {
         increment: (state) => { state.count += 1 },
-        decrement: (state) => { state.count -= 1 }
+        decrement: (state) => { state.count -= 1 },
+        reset: (state) => { state.count = 0 },
+
+        incrementByAmount: (state, actions) => {
+            state.count += actions.payload;
+
+        }
     }
 });
 
 // Both exports are needed (the actions, and the reducer itself).
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;
